3c91cc4173c668fdea7252a79ac3c0ee46bfd31f
youssefRamses <52958071+youssefRamses@users.noreply.github.com>
2023-12-04 19:17:16 +0200

    Add files via upload
    Slave 1 is the microcontroller controlled by the master that changes the color of the LED according to the sequence that the master decides using the push buttons

---
+++ Port_Cfg.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * Module: Port
+ * File Name: Port_Cfg.h
+ * Description: Pre-Compile Configuration Header file for TM4C123GH6PM Microcontroller - Port Driver
+ * Author: Team 8
+ ******************************************************************************/
+
+#ifndef PORT_CFG_H
+#define PORT_CFG_H
+
+
+/* Number of Configured Pins In the Mcu (MCU DEPENDENT) */
+#define PORT_ALL_PINS_NUMBER                  (48U)
+
+#endif /* PORT_CFG_H */
